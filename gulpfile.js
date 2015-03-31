e8f15dbcac7d37c2464241236b457cd54c34246f
Andres Cabrera <andrescabsi@gmail.com>
2015-03-31 00:00:09 -0500

    Animations, G Maps, Modal

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,27 @@ var gulp = require('gulp'),
 
 gulp.task('connect', function() {
   connect.server({
-    root: ['app', 'path'],
+    root: ['.', 'path'],
     port: 8080,
     livereload: true
   });
 });
 
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
+  gulp.src('app/*.html')
     .pipe(connect.reload());
 });
  
 gulp.task('less', function() {
-  gulp.src('./app/*.less')
+  gulp.src('app/less/*.less')
     .pipe(less())
-    .pipe(gulp.dest('./app/css'))
+    .pipe(gulp.dest('app/css'))
     .pipe(connect.reload());
 });
  
 gulp.task('watch', function() {
-	gulp.watch(['./app/*.html'], ['html']);
-    gulp.watch('./app/less/*.less', ['less']);
+	gulp.watch(['app/*.html'], ['html']);
+    gulp.watch('app/less/*.less', ['less']);
 })
  
 gulp.task('default', ['connect', 'html', 'less', 'watch']);

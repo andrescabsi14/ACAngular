bd43dd100322ef1857743a0441acf1ac9fd1aee5
Andres Cabrera <andrescabsi@gmail.com>
2015-04-12 23:25:20 -0500

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
-	connect = require('gulp-connect'),
-	less = require('gulp-less');
+  connect = require('gulp-connect'),
+  less = require('gulp-less'),
+  LessPluginCleanCSS = require('less-plugin-clean-css'),
+  LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+  cleancss = new LessPluginCleanCSS({ advanced: true }),
+  autoprefix = new LessPluginAutoPrefix({ browsers: ["last 5 versions"] });
 
 
 gulp.task('connect', function() {
@@ -18,13 +22,15 @@ gulp.task('html', function () {
  
 gulp.task('less', function() {
   gulp.src('app/less/*.less')
-    .pipe(less())
+    .pipe(less({
+      plugins: [autoprefix, cleancss]
+    }))
     .pipe(gulp.dest('app/css'))
     .pipe(connect.reload());
 });
  
 gulp.task('watch', function() {
-	gulp.watch(['app/*.html'], ['html']);
+  gulp.watch(['app/*.html'], ['html']);
     gulp.watch('app/less/*.less', ['less']);
 })
  
